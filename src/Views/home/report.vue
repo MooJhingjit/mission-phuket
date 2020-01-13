d68f38f55390db786cb5a487fea2a0adc5995478
pokkrong <pokkrong@dotpropertygroup.com>
2020-01-13 11:08:56 +0700

    Fix text report

--- src/Views/home/report.vue
+++ src/Views/home/report.vue
@@ -48,7 +48,7 @@
               <tr :key="index" v-for="(e, index) in item.responsibilities">
                 <!-- <td>{{item.department.name}}</td> -->
                 <td :style="{'border-bottom': (item.responsibilities.length <= 1) ? '0': '.05rem solid #dadee4'}">
-                  <div v-if="!e.answers.length" style="text-align: right;">ผู้รับผิดชอบ {{e.department.name}}</div>
+                  <div v-if="!e.answers.length" style="text-align: right;">{{e.department.name}}</div>
                   <table class="table" v-else>
                     <tr :key="index" v-for="(itemAns, index) in e.answers">
                       <td  :style="{'min-width': '400px', 'border-bottom': (e.answers.length <= 1) ? '0': '.05rem solid #dadee4'}">{{itemAns.cause}}</td>
